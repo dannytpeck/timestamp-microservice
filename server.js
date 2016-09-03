ad4febd95ed612067e31cd2748d2cac62c6c20c8
Danny Peck <dannytpeck@gmail.com>
2016-09-03 04:36:45 +0000

    Add mustache templating

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var mustacheExpress = require('mustache-express');
+
 var app = express();
 
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.render('index', { title: 'Timestamp Microservice' });
+});
+
+app.get('/:date', function (req, res) {
+  res.send(req.params);
 });
 
 app.listen(8080, function () {
